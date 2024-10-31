cd5cb39c5df7a8d09a4df40c0ecf82d6da38436a
JuanSantosElBisho <141331402+JuanSantosElBisho@users.noreply.github.com>
2024-10-30 23:31:16 -0500

    Efecto particulas monedas

--- Taller3Final/Assets/Moneda/Coin.cs
+++ Taller3Final/Assets/Moneda/Coin.cs
@@ -5,33 +5,32 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public static int coinCount = 0;
+    public GameObject particulasPrefab; // Prefab del sistema de partículas para la moneda
+
     // Start is called before the first frame update
     void Start()
     {
-        Coin.coinCount = Coin.coinCount + 1;
-        Debug.Log("Empieza el juego" + Coin.coinCount + "monedas");
-
-        
+        Coin.coinCount += 1;
+        Debug.Log("Empieza el juego con " + Coin.coinCount + " monedas");
     }
 
     private void OnTriggerEnter(Collider otherCollider)
     {
-        if (otherCollider.CompareTag("Player") == true)
+        if (otherCollider.CompareTag("Player"))
         {
+            // Instancia el sistema de partículas en la posición de la moneda
+            Instantiate(particulasPrefab, transform.position, Quaternion.identity);
 
+            Coin.coinCount--;
+            Debug.Log("Moneda recogida, quedan " + Coin.coinCount + " monedas");
+
+            if (Coin.coinCount == 0)
+            {
+                Debug.Log("Juego Terminado");
+            }
+
+            // Destruye la moneda
+            Destroy(gameObject);
         }
-
-        Coin.coinCount --;
-        Debug.Log("Moneda recogida" + Coin.coinCount + "monedas");
-    
-        if(Coin.coinCount == 0)
-        {
-            Debug.Log("Juego Terminado");
-        }
-
-        Destroy(gameObject);
-
     }
-
-
 }
